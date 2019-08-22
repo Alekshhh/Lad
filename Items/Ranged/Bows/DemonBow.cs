ac1402fdefb7c88ebee44634a2807947094b0ab7
Alekshhh <realmafioso3@gmail.com>
2019-08-22 17:44:56 +0300

    Update DemonBow.cs

--- Items/Ranged/Bows/DemonBow.cs
+++ Items/Ranged/Bows/DemonBow.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework;
 namespace Lad.Items.Ranged.Bows {
 	public class DemonBow : GlobalItem {
 		public virtual bool Shoot(Item item, Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
-			if (item.type == ItemID.DemonBow && item.ammo = ItemID.WoodenArrow) item.shoot = mod.ProjectileType("CustomCursed");	
+			if (item.type == ItemID.DemonBow && item.ammo == ItemID.WoodenArrow) item.shoot = mod.ProjectileType("CustomCursed");	
 			return true; 
 		}
 	}
